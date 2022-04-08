109e0970824261aeaec52a4cb046d83da65e5187
gonggr <101573574+gonggr@users.noreply.github.com>
2022-04-08 14:10:13 -0400

    Delete nlcon.m

--- nlcon.m
+++
@@ -1,9 +0,0 @@
-function [c,ceq]= nlcon(x)%%%% condiciones no lineales
-
-ceq = moment(x,1);   %%%% momento de primer orden 
-% c= sum(x);
-% ceq = sum(x);
-% c = sum(x.*2)-mean(x.*2);
-c=[];
-% ceq=[];
-end
