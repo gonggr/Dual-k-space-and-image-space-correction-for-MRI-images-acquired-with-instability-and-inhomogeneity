601ed4c7b6b8c8b1009bd3d0cb466e7c58e1adbc
gonggr <101573574+gonggr@users.noreply.github.com>
2022-04-08 14:10:53 -0400

    Add files via upload

---
+++ correction_prepare_get_data.m
@@ -0,0 +1,91 @@
+% ==========================================================================
+% function  : correction_prepare_get_data
+% --------------------------------------------------------------------------
+% purpose   : load the data  
+% input     : struct fsn
+% output    : struct fsn
+% comment   :
+% reference :   
+% --------------------------------------------------------------------------
+% 2022/03 - gonzalo.g.rodriguez@unc.edu.ar 
+% ==========================================================================
+
+function [fsn] = correction_prepare_get_data(fsn)
+    
+     disp('correction_prepare_get_data');
+        
+      if  fsn.param.correction_type == 0; % only instability correction: input 1 image
+            % ---- load data
+            fsn.data.folder = 'DataTest_Brain\';
+            load([fsn.data.folder 'img_instability.mat']); % data name / the image must be complex
+            img_1 = img_instability; % matrix name
+            [size_y_1,size_x_1] = size(img_1);     
+            
+            % ---- data normalization
+            img_1 = img_1./max(img_1(:));
+            
+            % ---- k-space
+            s0_1 = fft2(img_1);
+            
+            % ---- save data in structure
+            fsn.data.size_x_1 = size_x_1;
+            fsn.data.size_y_1 = size_y_1;
+            
+            fsn.data.img_1{1} = abs(img_1); % save abs value of img_1
+            
+            fsn.data.s0_1{1} = s0_1;
+            
+            % display initial image
+            figure;
+            imshow(fsn.data.img_1{1},[]);% title('data1');
+       
+      else % inhomogeneity or full correction: input 2 images (G+ & G-)
+          
+           % ---- load data
+           fsn.data.folder = 'DataTest_Brain\'; % Data folder
+           load([fsn.data.folder 'img_full_distorted_complex_1.mat']); % data 1 name
+           load([fsn.data.folder 'img_full_distorted_complex_2.mat']); % data 2 name
+           img_1 = img_full_distorted_complex_1; % name of the data inside the .mat 1
+           img_2 = img_full_distorted_complex_2; % name of the data inside the .mat 2
+                      
+           [size_x_1,size_y_1] = size(img_1);
+           [size_x_2,size_y_2] = size(img_2);   
+           
+            % ---- data normalization
+            img_1 = img_1./max(img_1(:));
+            img_2 = img_2./max(img_2(:));
+            
+            % ---- k-space
+            s0_1 = fft2(img_1);
+            s0_2 = fft2(img_2); 
+            
+            % ---- save data in structure
+            fsn.data.size_x_1 = size_x_1;
+            fsn.data.size_y_1 = size_y_1;
+            fsn.data.size_x_2 = size_x_2;
+            fsn.data.size_y_2 = size_y_2;
+            
+            fsn.data.img_1{1} = abs(img_1);
+            fsn.data.img_2{1} = abs(img_2);
+            
+            fsn.data.s0_1{1} = s0_1;
+            fsn.data.s0_2{1} = s0_2;
+            
+            if fsn.param.display_intermediate_results == 1;
+            % ---- display initial images
+            figure;
+            subplot(1,2,1); imshow(fsn.data.img_1{1},[]); title('data 1');
+            subplot(1,2,2); imshow(fsn.data.img_2{1},[]); title('data 2');
+            end
+  
+      end     
+end      
+
+
+
+
+
+
+
+
+
